b756ee29e07a2579ecb7befd2659bf53455ef9ec
LongNT2000 <timhieu12345678@gmail.com>
2021-10-10 01:31:36 +0700

    test husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@ const formRouter = require('./form');
 
 function route(app) {
     app.use('/fu', formRouter);
-                                    app.get('/', function (req, res) {
+    app.get('/', function (req, res) {
         res.render('home');
     });
-    app.get('/form', function (req, res) {
-                res.render('form');
-    });
+        app.get('/form', function (req, res) {
+            res.render('form');
+        });
     app.post('/form', function (req, res) {
-                                        console.log(req.body);
-                                        res.render('form');
-                                    });
+                            console.log(req.body);
+                            res.render('form');
+                        });
 }
-module.exports = route;
+module.exports = route; 
